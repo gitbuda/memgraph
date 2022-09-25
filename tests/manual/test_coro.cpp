09599745a00497360b198fc30f3a6306647d73cb
Marko Budiselic <mbudiselicbuda@gmail.com>
2022-09-25 14:57:49 +0200

    Add more realistic example

--- tests/manual/test_coro.cpp
+++ tests/manual/test_coro.cpp
@@ -31,45 +31,40 @@ template <typename T>
 struct SyncGenerator {
   struct promise_type;
   using handle_type = std::experimental::coroutine_handle<promise_type>;
+  handle_type h_;
 
-  struct promise_type {  // required
+  struct promise_type {
     T value_;
     std::exception_ptr exception_;
-
     SyncGenerator get_return_object() { return SyncGenerator(handle_type::from_promise(*this)); }
     std::experimental::suspend_always initial_suspend() { return {}; }
     std::experimental::suspend_always final_suspend() noexcept { return {}; }
-    void unhandled_exception() {
-      exception_ = std::current_exception();
-    }  // saving
-       // exception
-
-    // template <std::convertible_to<T> From>  // C++20 concept
+    void unhandled_exception() { exception_ = std::current_exception(); }
     template <typename From>
     std::experimental::suspend_always yield_value(From &&from) {
+      std::cout << "yield value RVALUE" << std::endl;
       value_ = std::forward<From>(from);  // caching the result in promise
       return {};
     }
-    void return_void() {}
+    void return_value(T &&value) {
+      value_ = std::forward<T>(value);
+      return;
+    }
+    void return_value(const T &value) {
+      value_ = value;
+      return;
+    }
   };
-
-  handle_type h_;
 
   SyncGenerator(handle_type h) : h_(h) {}
   ~SyncGenerator() { h_.destroy(); }
   explicit operator bool() {
-    fill();  // The only way to reliably find out whether or not we finished coroutine,
-             // whether or not there is going to be a next value generated (co_yield) in
-             // coroutine via C++ getter (operator () below) is to execute/resume coroutine
-             // until the next co_yield point (or let it fall off end).
-             // Then we store/cache result in promise to allow getter (operator() below to
-             // grab it without executing coroutine).
+    fill();
     return !h_.done();
   }
   T operator()() {
     fill();
-    full_ = false;  // we are going to move out previously cached
-                    // result to make promise empty again
+    full_ = false;
     return std::move(h_.promise().value_);
   }
 
@@ -79,37 +74,29 @@ struct SyncGenerator {
     if (!full_) {
       h_();
       if (h_.promise().exception_) std::rethrow_exception(h_.promise().exception_);
-      // propagate coroutine exception in called context
       full_ = true;
     }
   }
 };
 
-SyncGenerator<uint64_t> fibonacci_sequence(unsigned n) {
-  if (n == 0) co_return;
-  if (n > 94) throw std::runtime_error("Too big Fibonacci sequence. Elements would overflow.");
-
-  co_yield 0;
-  if (n == 1) co_return;
-  co_yield 1;
-  if (n == 2) co_return;
-
-  uint64_t a = 0;
-  uint64_t b = 1;
-  for (unsigned i = 2; i < n; i++) {
-    uint64_t s = a + b;
-    co_yield s;
-    a = b;
-    b = s;
+SyncGenerator<int> CreateDummyGenerator(int *ptr, int max) {
+  for (int i = 0; i < max; ++i) {
+    std::cout << "Call i: " << i << std::endl;
+    co_yield i;
   }
+  co_return max;
 }
 
 int main() {
   try {
-    auto gen = fibonacci_sequence(10);  // max 94 before uint64_t overflows
-
-    for (int j = 0; gen; j++) std::cout << "fib(" << j << ")=" << gen() << '\n';
-
+    auto ptr = std::make_unique<int>(10);
+    auto gen1 = CreateDummyGenerator(ptr.get(), 10);
+    for (; gen1;) {
+      auto gen2 = CreateDummyGenerator(ptr.get(), 5);
+      for (; gen2;) {
+        std::cout << gen1() * gen2() << std::endl;
+      }
+    }
   } catch (const std::exception &ex) {
     std::cerr << "Exception: " << ex.what() << '\n';
   } catch (...) {
