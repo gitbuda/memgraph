ed6fe4bad7fd6abde75f2a504ed75868aab48c8e
Marko Budiselic <mbudiselicbuda@gmail.com>
2024-03-12 00:46:54 +0000

    Add storage high-level ideas, part 1

--- src/storage/custom_storage/storage.hpp
+++ src/storage/custom_storage/storage.hpp
@@ -13,6 +13,27 @@
 
 namespace memgraph::storage::custom_storage {
 
+// Desing ideas:
+//   * try to follow existing API design
+//     * one of the issue is granular and SYNC API design
+//   * try to reuse exisitng in-memory data structures
+//     * PropertyStore + all PropertyValues seem very reusable
+//     * Vertex/Edge seems not very reusable because (delta*, edges*)
+//   * parallelization
+//   * ASYNC disk/network access
+//   * per database, maybe even isolated data cache
+
+// Target queries:
+//   CREATE (:Label {props});                                                            // single vertex create
+//   UNWIND vertices_props AS props CREATE (n:Label) SET n += props;                     // batch  vertex create
+//   MATCH (n:Label {id:X}) RETURN n;                                                    // single vertex lookup
+//   MATCH (n1:Label {id:X}) MATCH (n2:Label {id:Y}) CREATE (n1)-[r:Type {props}]->(n2); // single edge   create
+//   // batch edge create
+//   // iterate all verteices with limited memory usage -> "global" graph algos possible
+//   // iterate all edges with limited memory usage     -> "global" graph algos possible
+//   // get IN/OUT/ALL edges for a given vertex         -> "global" graph algos possible
+//   // BFS with filter lambda
+
 class Storage {
  public:
   void Call();
